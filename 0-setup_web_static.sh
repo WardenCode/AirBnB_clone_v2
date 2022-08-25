3d57acf5b13fdde99a04e5183c1d56e4e5d91e02
Diego Linares <diegojeanluck@hotmail.com>
2022-08-25 12:00:29 -0500

    Fix: put the line of chown in the right place (to follow sym links)

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,11 +3,10 @@
 sudo apt -y update
 sudo apt -y upgrade
 sudo apt -y install nginx
-sudo service nginx start
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-sudo chown -hR ubuntu:ubuntu /data/
 echo "New Content" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
 
 find_str="^\t\}$"
 replace_str="\t\}\n\n\tlocation \/hbnb_static\/ \{\n\t\talias \/data\/web_static\/current\/;\n\t\}"
