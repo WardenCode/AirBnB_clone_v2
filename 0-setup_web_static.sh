52c96c47f4f7d296a70d52edbfb47395057c7cdc
Diego Linares <diegojeanluck@hotmail.com>
2022-08-25 11:45:12 -0500

    Feat 'Deploy Static': mandatory task #0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,3 +1,16 @@
-#!/usr/bin/env bash
-#
+#!/usr/bin/env bash                                              
+# Sets up your web servers for the deployment of web_static      
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt -y install nginx
+sudo service nginx start
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+sudo chown -R ubuntu:ubuntu /data/
+echo "New Content" | tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test /data/web_static/current
 
+find_str="^\t\}$"
+replace_str="\t\}\n\n\tlocation \/hbnb_static\/ \{\n\t\talias \/data\/web_static\/current\/;\n\t\}"
+
+sudo sed -i "0,/$find_str/s//$replace_str/" /etc/nginx/sites-available/default
+sudo service nginx restart
