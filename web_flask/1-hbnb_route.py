e7026fd2a17eecfc43fa84df38032f9634ada709
Diego Linares <diegojeanluck@hotmail.com>
2022-09-11 02:23:32 -0500

    Fix pycodestyle

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,15 +4,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_world():
     """Display a standard message on get petition"""
     return ('Hello HBNB!')
 
+
 @app.route("/hbnb", strict_slashes=False)
 def main():
     """Display HBNB!"""
     return ('HBNB!')
+
 
 if (__name__ == '__main__'):
     app.run(host="0.0.0.0", port=5000)
