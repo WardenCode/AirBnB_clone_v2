72605c459650a7b4ae2391cec23456c0c26392ff
Diego Linares <diegojeanluck@hotmail.com>
2022-09-11 02:16:11 -0500

    Feat mandatory task #1 and works

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Initialize a Flask application with two routes"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """Display a standard message on get petition"""
+    return ('Hello HBNB!')
+
+@app.route("/hbnb", strict_slashes=False)
+def main():
+    """Display HBNB!"""
+    return ('HBNB!')
+
+if (__name__ == '__main__'):
+    app.run(host="0.0.0.0", port=5000)
